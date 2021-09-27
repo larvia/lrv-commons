c6bca48a38b7313e97d9bf823df1dce3302ece05
Andres Tapia <andres10-93@hotmail.com>
2021-09-27 09:11:00 -0500

    Update commons.ts

--- src/commons.ts
+++ src/commons.ts
@@ -4,15 +4,17 @@ export const roles = {
   CLIENT_OWNER: 'CLIENT_OWNER',
   CLIENT_MANAGER: 'CLIENT_MANAGER',
   CLIENT_OPERATOR: 'CLIENT_OPERATOR',
+  FINANCE: 'FINANCE',
 };
 
 export function getHigherRole(userRoles: Array<String>) {
   const rolesScore = {
-    [roles.SUPER_ADMIN]: 5,
-    [roles.SALES]: 4,
-    [roles.CLIENT_OWNER]: 3,
-    [roles.CLIENT_MANAGER]: 2,
-    [roles.CLIENT_OPERATOR]: 1
+    [roles.SUPER_ADMIN]: 6,
+    [roles.SALES]: 5,
+    [roles.CLIENT_OWNER]: 4,
+    [roles.CLIENT_MANAGER]: 3,
+    [roles.CLIENT_OPERATOR]: 2,
+    [roles.FINANCE]: 1,
   };
   const scores = userRoles.map((r: any) => rolesScore[r]);
   const higherRoleScore = Math.max(...scores);
@@ -21,14 +23,14 @@ export function getHigherRole(userRoles: Array<String>) {
 }
 
 export const languages = {
-  ES : 'Español',
-  EN : 'English'
+  ES: 'Español',
+  EN: 'English'
 }
 
 
 export const analysisTypes = [
   'GENERAL_LARVAE_ANALYSIS'
-  ]
+]
 
 export const larvaeSpecies = [
   'LITOPENAEUS_VANNAMEI'
